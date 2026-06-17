05a2f49b66895e55413f188cf09bc7323d77ab2d
Dheeraj611 <dhe06sharma@gmail.com>
2026-06-17 21:02:41 +0530

    Add two_number.py

--- Day01/two_number.py
+++ Day01/two_number.py
@@ -1,9 +1,7 @@
 class Solution:
     def addTwoNumbers(self, l1, l2):
-
         head = ListNode(0)
         temp = head
-
         carry = 0
 
         while l1 is not None or l2 is not None or carry > 0:
@@ -20,11 +18,10 @@ class Solution:
                 l2 = l2.next
 
             total = num1 + num2 + carry
-
             carry = total // 10
 
-            newNode = ListNode(total % 10)
-            temp.next = newNode
+            new_node = ListNode(total % 10)
+            temp.next = new_node
             temp = temp.next
 
         return head.next
